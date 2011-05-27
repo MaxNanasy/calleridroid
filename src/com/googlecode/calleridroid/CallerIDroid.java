951d9ef2a4e5ec736351a4da4c156bb323b0e68d
Max Nanasy <madmardigan53@gmail.com>
2011-05-27 02:39:37 -0700

    Added broadcast receiver with rudimentary caller id.

--- src/com/googlecode/calleridroid/CallerIDroid.java
+++ src/com/googlecode/calleridroid/CallerIDroid.java
@@ -1,14 +1,20 @@
 package com.googlecode.calleridroid;
 
-import android.app.Activity;
-import android.os.Bundle;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
 
-public class CallerIDroid extends Activity {
+public class CallerIDroid extends BroadcastReceiver {
     
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(TelephonyManager.ACTION_PHONE_STATE_CHANGED)) {
+            if (intent.getStringExtra(TelephonyManager.EXTRA_STATE).equals(TelephonyManager.EXTRA_STATE_RINGING)) {
+                Toast.makeText(context, "You are receiving a phone call from " + intent.getStringExtra(TelephonyManager.EXTRA_INCOMING_NUMBER), Toast.LENGTH_LONG).show();
+            }
+        }
     }
     
 }
